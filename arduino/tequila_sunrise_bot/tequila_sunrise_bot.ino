c7cfd6c7b073b6fbc72ae5a3d47bb04ae5a7862e
Robert Kaye <rob@musicbrainz.org>
2012-01-19 15:34:25 -0800

    First cut of the arduino sketch for the tsb. Committing on a plane!

---
+++ arduino/tequila_sunrise_bot/tequila_sunrise_bot.ino
@@ -0,0 +1,149 @@
+
+// 
+// Copyright (c) Party Robotics LLC 2012
+// Written by Robert Kaye <rob@partyrobotics.com>
+//
+
+// ---------------------------------------------
+// defintions for the TSB motors
+// ---------------------------------------------
+const int motor_count = 3;
+class motor_cmd
+{
+    public:
+    
+        int8_t   motor;
+        uint16_t duration;
+};
+
+// ---------------------------------------------
+// debugging support stuff
+// ---------------------------------------------
+
+#define MAX 80
+// debugging printf function. Max MAX characters per line!!
+void dprintf(const char *fmt, ...)
+{
+    va_list va;
+    va_start (va, fmt);
+    char buffer[MAX];
+    char *ptr = buffer;
+    vsnprintf(buffer, MAX, fmt, va);
+    va_end (va);
+    Serial.println(buffer);
+}
+
+
+
+// the proportions of the drink expressed in ms of motoro run time
+static motor_cmd drinks[2][3] =
+{
+    { // Normal
+        { 0, 3560 },
+        { 1, 21360 },
+        { 2, 10680 }
+    },
+    { // Strong
+        { 0, 3830 },
+        { 1, 19150 },
+        { 2, 12770 }
+    }
+};
+
+// ---------------------------------------------
+// TSB driver code
+// ---------------------------------------------
+
+// compare function for qsort. Sorts by duration
+int compare(const void *a, const void *b)
+{
+    return ((motor_cmd *)a)->duration > ((motor_cmd *)b)->duration;
+}
+
+// call this function to make a drink. You must pass in up to 
+// MOTOR_COUNT (3) number of motor_cmds
+void make_drink(uint8_t count, motor_cmd *cmds)
+{
+    uint8_t  i;
+    uint16_t t = 0, duration;
+
+    if (count > motor_count)
+        return;
+
+    // sort by duration
+//    qsort(cmds, count, sizeof(motor_cmd), compare);
+
+//    for(i = 0; i < count; i++)
+//        dprintf("%05u: motor %d on, %u ms\n", t, cmds[i].motor, cmds[i].duration);
+    
+    // turn motors on
+    for(i = 0; i < count; i++)
+    {
+        if (cmds[i].motor == 0)
+            digitalWrite(8, HIGH);
+        if (cmds[i].motor == 1)
+            digitalWrite(9, HIGH);
+        if (cmds[i].motor == 2)
+            digitalWrite(10, HIGH);
+    }
+
+    // wait the appropriate amount of time and turn off motors
+    // starting with the shortest duration
+    for(i = 0; i < count; i++)
+    {
+        duration = cmds[i].duration - t;
+        dprintf("delay: %d\n", duration);
+        while(duration > 0)
+        {
+            uint8_t j;
+            uint8_t d = duration > 10 ? 10 : duration;
+
+            delay(d);
+            duration -= d;
+            t += d;
+        }
+        if (cmds[i].motor == 0)
+            digitalWrite(8, LOW);
+        if (cmds[i].motor == 1)
+            digitalWrite(9, LOW);
+        if (cmds[i].motor == 2)
+            digitalWrite(10, LOW);
+    }
+}
+
+void setup()
+{
+    Serial.begin(38400);
+
+    // Set motor pins as outputs
+    pinMode(8, OUTPUT); 
+    pinMode(9, OUTPUT); 
+    pinMode(10, OUTPUT); 
+
+    // set the input pins and enable the pull up resistors
+    pinMode(2, INPUT); 
+    pinMode(3, INPUT); 
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+}   
+    
+void loop()
+{
+
+    dprintf("\nTequila Sunrise bot. What shall be your bidding?\n\n");
+    for(;;)
+    {
+        if (digitalRead(2) == LOW)
+        {
+            dprintf("making normal drink\n");
+            //make_drink(3, drinks[0]);
+            dprintf("drink complete. bottoms up!\n");
+        }
+        if (digitalRead(3) == LOW)
+        {
+            dprintf("making strong drink\n");
+            //make_drink(3, drinks[1]);
+            dprintf("drink complete. bottoms up!\n");
+        }
+    }
+}
